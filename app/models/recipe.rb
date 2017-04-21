3c12089102cc4cb28e636d8fa9338773562f1fd1
zhileiz <zhileiz@seas.upenn.edu>
2017-04-21 10:40:13 -0400

    1st stylized

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,9 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :directions, inverse_of: :recipe
   has_many :ingredients, inverse_of: :recipe
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   has_attached_file :image, styles: { medium: "400x400#>" }
