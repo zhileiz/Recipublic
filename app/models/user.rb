e21ab7a520e8d0780cc140aec610d6ab2c9a339b
zhileiz <zhileiz@seas.upenn.edu>
2017-04-23 16:48:03 -0400

    User Links

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, uniqueness: true
   has_many :recipes, dependent: :destroy
-  has_attached_file :avatar, styles: { medium: "400x400#>" }
+  has_attached_file :avatar, styles: { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "empty_user.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
